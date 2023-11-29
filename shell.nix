24448c9e692cef76d9348fae44e14d43fa44f8c6
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-11-29 15:29:56 +0000

    got the app working LEZGO

--- shell.nix
+++ shell.nix
@@ -8,9 +8,9 @@ let
 	my-python = pkgs.python39.withPackages packages;
 in
 pkgs.mkShell {
-	buildInputs = with pkgs; [
-		# qt5
+	buildInputs = [
 		my-python
-		xorg.libXinerama
 	];
+
+	QT_QPA_PLATFORM_PLUGIN_PATH="${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
 }
