c15d0ec608e2252a7471804751f9406bb1be1928
Filipe Ligeiro Silva <57916727+filipelsilva@users.noreply.github.com>
2023-12-13 16:04:30 +0000

    move by scp wav

--- src/send.sh
+++ src/send.sh
@@ -1,3 +1,3 @@
 echo "Sending audio files to ELMO..."
 address="192.168.0.$1"
-scp audio/*.mp3 idmind@"$address":elmo-v2/src/static/sounds
+scp audio/*.wav idmind@"$address":elmo-v2/src/static/sounds
